aefb33556b6083eaa648a3f6a1918dacba75d34b
atsushi-nonaka <atsushi.nonaka@rakus-partners.co.jp>
2020-03-17 07:40:06 +0900

    ユーザー登録するための機能を追加しました

---
+++ src/main/java/com/example/service/RegisterUserService.java
@@ -0,0 +1,31 @@
+package com.example.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.domain.User;
+import com.example.repository.RegisterUserRepository;
+
+/**
+ * ユーザー登録するためのサービス.
+ * 
+ * @author nonaa
+ *
+ */
+@Service
+@Transactional
+public class RegisterUserService {
+	
+	@Autowired
+	private RegisterUserRepository repository;
+	
+	/**
+	 * ユーザー登録するためにレポジトリを呼び出す.
+	 * 
+	 * @param user ユーザー
+	 */
+	public void registerUser(User user) {
+		repository.insert(user);
+	}
+}
